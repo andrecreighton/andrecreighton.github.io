b1bf99a1ab5c0e6c8aa68601fc098cc07924fb5d
Andre Creighton <acreighton92@gmail.com>
2021-05-24 17:50:53 -0400

    Added pages to website

---
+++ cisc3610/public_html/scripts/audio.js
@@ -0,0 +1,37 @@
+var baby = new Audio();
+baby.src = "./audio/baby.mp3";
+
+var horse = new Audio();
+horse.src = "./audio/horse.mp3"
+
+var car = new Audio();
+car.src = "./audio/car.mp3"
+
+var ambulance = new Audio();
+ambulance.src = "./audio/ambulance.mp3"
+
+var fireworks = new Audio();
+fireworks.src = "./audio/fireworks.mp3"
+
+
+function playBaby() {
+
+    baby.play();
+}
+
+function playHorse() {
+    horse.play();
+}
+
+function playCar() {
+    car.play();
+}
+
+function playAmbulance() {
+    ambulance.play();
+}
+
+function playFireworks() {
+    fireworks.play();
+
+}
